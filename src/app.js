974ef1f4ecf201ab59b1378b2c7dd7ede9ffbbb8
Alicia-Souza <aliciadesouzamiranda360@gmail.com>
2025-09-19 11:41:37 -0300

    tada: init: criação do arquivo app.js

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'React';
+import './App.css'
+
+function App(){
+    return(
+        //Arquivo principal
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello Worlddd!</h1>
+                <p></p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
